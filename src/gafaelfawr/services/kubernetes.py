b6314db5d1376385105b8a54c3e31b5da3496d3b
Russ Allbery <rra@lsst.org>
2021-05-03 08:58:08 -0700

    WIP

--- src/gafaelfawr/services/kubernetes.py
+++ src/gafaelfawr/services/kubernetes.py
@@ -6,21 +6,30 @@ from base64 import b64decode
 from typing import TYPE_CHECKING
 
 from gafaelfawr.config import KubernetesConfig
-from gafaelfawr.exceptions import KubernetesError
+from gafaelfawr.exceptions import (
+    KubernetesError,
+    PermissionDeniedError,
+    ValidationError,
+)
 from gafaelfawr.models.token import (
     AdminTokenRequest,
     Token,
     TokenData,
     TokenType,
 )
-from gafaelfawr.storage.kubernetes import SecretType
+from gafaelfawr.storage.kubernetes import StatusReason
 
 if TYPE_CHECKING:
+    from typing import List
+
+    from kubernetes.client import V1Secret
     from structlog.stdlib import BoundLogger
 
-    from gafaelfawr.config import ServiceSecret
     from gafaelfawr.services.token import TokenService
-    from gafaelfawr.storage.kubernetes import KubernetesStorage
+    from gafaelfawr.storage.kubernetes import (
+        GafaelfawrServiceToken,
+        KubernetesStorage,
+    )
 
 __all__ = ["KubernetesService"]
 
@@ -48,10 +57,7 @@ class KubernetesService:
         self._logger = logger
 
     async def update_service_secrets(self) -> None:
-        """Ensure all configured service secrets exist and are valid.
-
-        Removes any secrets found in Kubernetes with the appropriate label
-        that are not part of the configured set.
+        """Ensure all GafaelfawrServiceToken secrets exist and are valid.
 
         Raises
         ------
@@ -60,137 +66,114 @@ class KubernetesService:
             processing single secrets will be logged but this method will
             attempt to continue processing the remaining secrets.
         """
-        wanted = {
-            (s.secret_name, s.secret_namespace): s
-            for s in self._config.service_secrets
-        }
         try:
-            secrets = self._storage.list_secrets(SecretType.service)
+            service_tokens = self._storage.list_service_tokens()
         except KubernetesError as e:
             # Report this error even though it's unrecoverable and we're
             # re-raising it, since our caller doesn't have the context that
-            # the failure was due to listing service token secrets.
-            msg = "Unable to list service token secrets"
+            # the failure was due to listing GafaelfawrServiceToken objects.
+            msg = "Unable to list GafaelfawrServiceToken objects"
             self._logger.error(msg, error=str(e))
             raise
 
-        # Remove any secrets that shouldn't exist and update any secrets that
-        # already exist.
-        for secret in secrets:
-            name = secret.metadata.name
-            namespace = secret.metadata.namespace
-            if (name, namespace) not in wanted:
-                self._delete_service_secret(name, namespace)
-            else:
-                await self._update_service_secret(wanted[(name, namespace)])
-                del wanted[(name, namespace)]
+        # Process each GafaelfawrServiceToken and create or update its
+        # corresponding secret if needed.
+        for service_token in service_tokens:
+            await self._update_service_secret(service_token)
 
-        # Create any secrets that we're missing.
-        for missing_secret in wanted.values():
-            await self._create_service_secret(missing_secret)
+    async def _check_service_secret(
+        self, parent: GafaelfawrServiceToken, secret: V1Secret
+    ) -> bool:
+        """Check if a secret needs to be updated."""
+        okay = (
+            secret.metadata.annotations == parent.annotations
+            and secret.metadata.labels == parent.labels
+            and "token" in secret.data
+        )
+        if not okay:
+            return False
+
+        # Check the token contained in the secret.
+        try:
+            token_str = b64decode(secret.data["token"]).decode()
+            token = Token.from_str(token_str)
+            okay = await self._check_service_token(token, parent)
+        except Exception:
+            okay = False
+
+        return okay
 
     async def _check_service_token(
-        self, token: Token, service_secret: ServiceSecret
+        self, token: Token, parent: GafaelfawrServiceToken
     ) -> bool:
         """Check whether a service token matches its configuration."""
         token_data = await self._token_service.get_data(token)
         if not token_data:
             return False
-        if token_data.username != service_secret.service:
+        if token_data.username != parent.service:
             return False
-        if sorted(token_data.scopes) != sorted(service_secret.scopes):
+        if sorted(token_data.scopes) != sorted(parent.scopes):
             return False
         return True
 
-    async def _create_service_secret(
-        self, service_secret: ServiceSecret
-    ) -> None:
-        """Create a Kubernetes service secret."""
-        token = await self._create_service_token(service_secret)
-        name = service_secret.secret_name
-        namespace = service_secret.secret_namespace
-        try:
-            self._storage.create_secret(
-                name, namespace, SecretType.service, token
-            )
-        except KubernetesError as e:
-            msg = f"Creating {namespace}/{name} failed"
-            self._logger.error(msg, error=str(e))
-        else:
-            self._logger.info(
-                f"Created {namespace}/{name} secret",
-                service=service_secret.service,
-                scopes=service_secret.scopes,
-            )
-
     async def _create_service_token(
-        self, service_secret: ServiceSecret
+        self, service: str, scopes: List[str]
     ) -> Token:
         request = AdminTokenRequest(
-            username=service_secret.service,
+            username=service,
             token_type=TokenType.service,
-            scopes=service_secret.scopes,
+            scopes=scopes,
         )
         return await self._token_service.create_token_from_admin_request(
             request, TokenData.internal_token(), ip_address=None
         )
 
-    def _delete_service_secret(self, name: str, namespace: str) -> None:
-        """Delete a Kubernetes service secret."""
-        try:
-            self._storage.delete_secret(name, namespace, SecretType.service)
-        except KubernetesError as e:
-            msg = f"Deleting {namespace}/{name} failed"
-            self._logger.error(msg, error=str(e))
-        else:
-            self._logger.info(f"Deleted {namespace}/{name} secret")
-
     async def _update_service_secret(
-        self, service_secret: ServiceSecret
+        self, parent: GafaelfawrServiceToken
     ) -> None:
         """Verify that a service secret is still correct.
 
-        This checks that the service token is still valid and replaces it with
-        a new one if not.
+        This checks that the contained service token is still valid and the
+        secret metadata matches the GafaelfawrServiceToken metadata and
+        replaces it with a new one if not.
         """
-        name = service_secret.secret_name
-        namespace = service_secret.secret_namespace
+        name = parent.secret_name
+        namespace = parent.secret_namespace
         try:
-            secret = self._storage.get_secret(
-                name, namespace, SecretType.service
-            )
+            secret = self._storage.get_service_secret(parent)
         except KubernetesError as e:
             msg = f"Updating {namespace}/{name} failed"
             self._logger.error(msg, error=str(e))
             return
-        if not secret:
-            self._logger.error(
-                f"Updating {namespace}/{name} failed",
-                error=f"Secret {namespace}/{name} not found while updating",
-            )
+        if secret and await self._check_service_secret(parent, secret):
             return
 
-        valid = False
-        if "token" in secret.data:
-            try:
-                token_str = b64decode(secret.data["token"]).decode()
-                token = Token.from_str(token_str)
-                valid = await self._check_service_token(token, service_secret)
-            except Exception:
-                valid = False
-        if valid:
-            return
-
-        # The token is not valid.  Replace the secret.
-        token = await self._create_service_token(service_secret)
+        # Something is either different or invalid.  Replace the secret.
         try:
-            self._storage.patch_secret(name, namespace, token)
+            token = await self._create_service_secret(parent)
+            if secret:
+                self._storage.replace_service_secret(parent, token)
+            else:
+                self._storage.create_service_secret(parent, token)
+        except (PermissionDeniedError, ValidationError) as e:
+            msg = f"Updating {namespace}/{name} failed"
+            self._logger.error(msg, error=str(e))
+            try:
+                self._storage.update_service_token_status(
+                    parent,
+                    reason=StatusReason.Failed,
+                    message=str(e),
+                    success=False,
+                )
+            except KubernetesError as e:
+                msg = f"Updating status of {namespace}/{name} failed"
+                self._logger.error(msg, error=str(e))
         except KubernetesError as e:
             msg = f"Updating {namespace}/{name} failed"
             self._logger.error(msg, error=str(e))
         else:
             self._logger.info(
                 f"Updated {namespace}/{name} secret",
-                service=service_secret.service,
-                scopes=service_secret.scopes,
+                service=parent.service,
+                scopes=parent.scopes,
             )
